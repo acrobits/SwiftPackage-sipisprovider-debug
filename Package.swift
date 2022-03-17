b5fa3d069b76ab0673e6f7067db1ff9809377e21
Stanislav Kutil <skutil@acrobits.cz>
2022-03-17 22:07:45 +0100

    Package updated to 173723

--- Package.swift
+++ Package.swift
@@ -15,6 +15,6 @@ let package = Package(
         .binaryTarget(
             name: "SipisProvider",
             url: "https://dist.acrobits.net/saas/sipis-provider-swift-package/debug/SipisProvider.xcframework-173723.zip",
-            checksum: "5e43d776476c7bc826a802424f81171a767165c7829c6fd76a7253b1133308e4"),
+            checksum: "686c48d77f30bbc82c65c2d383cf6b95db524bb217bef2f966ae89633e21d732"),
     ]
 )
